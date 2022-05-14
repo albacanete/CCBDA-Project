5de36aa8f6bd523af5ddab4b77f55afe1771a8fe
Marc Manzanares <marc.manzanares@upc.edu>
2022-05-14 12:48:08 +0200

    Added django+postgresql DB configuration

--- backend/djangoProject3/settings.py
+++ backend/djangoProject3/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'ccbda_project.apps.CcbdaProjectConfig',
     #'rest_framework',
     'djangoProject3',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +81,13 @@ WSGI_APPLICATION = 'djangoProject3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DJANGO_DB_ENGINE', 'django.db.backends.postgresql_psycopg2'),
+        'NAME': os.environ.get('DJANGO_DB_NAME', 'ccbda-project'),
+        'USER': os.environ.get('DJANGO_DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', 'admin'),
+        'HOST': os.environ.get('DJANGO_DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DJANGO_DB_PORT', 5432),
+        'ATOMIC_REQUESTS': True
     }
 }
 
