6b9f93720b48d51993800bbdc92fe34a583f167a
oriolmartinezac <omartinez.roser@gmail.com>
2022-05-26 17:49:30 +0200

    login/register changed

--- backend/ccbda_project/static/custom/js/custom-script.js
+++ backend/ccbda_project/static/custom/js/custom-script.js
@@ -1,17 +1,4 @@
-
-/*
-document.getElementsByTagName('select')[0].onchange = function() {
-  var index = this.selectedIndex;
-  var inputText = this.children[index].innerHTML.trim();
-  console.log(inputText);
-}
-*/
-
-
-
 function selectFunction(item) {
-
-
   var item_id = item.id;
   var item_value = item.value;
   var number_id = parseInt(item_id.toString().split("_")[2]);
@@ -23,11 +10,9 @@ function selectFunction(item) {
       for (var i = 1; i < total_hide+1; i++) {
         document.getElementById(text.concat(number_id + i).toString()).style.display = "none";
         document.getElementById(text.concat(number_id + i).toString()).value ="-1";
-
       }
     } else {
       document.getElementById(text.concat(number_id + 1).toString()).style.display = "block";
     }
-
   }
 }
