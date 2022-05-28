40bfb04863e449dfecd255f5fe9ee6c284eca741
LeixB <abone9999@gmail.com>
2022-05-28 12:10:57 +0200

    Optimize db import using bulk_create and csv files

--- backend/djangoProject3/management/commands/parseData.py
+++ backend/djangoProject3/management/commands/parseData.py
@@ -2,62 +2,30 @@ from django.core.management.base import BaseCommand
 from djangoProject3.models import Player_Status
 from djangoProject3.models import Team_Status
 
-import re
-import json
+import csv
+import os
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        f= open("/home/oriol/Escritorio/CCBDA/CCBDA-Project/scrapy/transfermarkt.json")
-        data= json.load(f)
-        for i in data:
-            #Team
-            if(len(i) <6):
-                nameTeam=i['squad_name']
-                year=i['year']
-                avgAge=i['avg_age']
-                if(avgAge==-1):
-                    print("avgAge")
-                valueTeam = i['squad_value']
-                numberPlayers = i['number_players']
-                championship=i['championship']
-                Team_Status.objects.create(nameTeam = nameTeam, nameLeague= championship, year = year,avgAge= avgAge,valueTeam = valueTeam, numberPlayers = numberPlayers)
-            #Player
-            elif (len(i) >9):
-                namePlayer=i['name_player']
-                age=i['age']
-                role = i['role']
-                year=i['year']
-                valuePlayer = i['value_player']
-                nameTeam = i['squad_name']
-                games=i['games_played']
-                minutes = i['minute_played']
-                championship = i['championship']
-                if(i['role']=="Goalkeeper"):
-                    cleanSheets=i['clean_sheets']
-                    goalsConceded = i['goals_conceded']
-                    goals=0
-                    assists=0
-                else:
-                    cleanSheets = 0
-                    goalsConceded = 0
-                    goals =i['goals']
-                    assists = i['assists']
-                Player_Status.objects.create(namePlayer = namePlayer,
-                                            nameTeam = nameTeam,
-                                            nameLeague= championship,
-                                            age = age,
-                                            year = year,
-                                            valuePlayer = valuePlayer,
-                                            role = role,
-                                            games = games,
-                                            goals = goals,
-                                            assists = assists,
-                                            minutes = minutes,
-                                            goalsConceded = goalsConceded,
-                                            cleanSheets = cleanSheets)
 
-            # Closing file
-        f.close()
+        # Get the path to the data folder
+        data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+
+        with open(os.path.join(data_path, 'squads.csv'), 'r') as f:
+            teams = Team_Status.objects.bulk_create(map(lambda row: Team_Status(**row), csv.DictReader(f)))
+
+        print(f"Team_Status table populated: {len(teams)} rows")
+
+        def parsePlayerRow(row: dict) -> dict:
+            for san in ["goalsConceded", "goals", "assists", "cleanSheets"]:
+                if row[san] == "":
+                    row[san] = 0
+            return Player_Status(**row)
+
+        with open(os.path.join(data_path, 'players.csv'), 'r') as f:
+            players = Player_Status.objects.bulk_create(map(parsePlayerRow, csv.DictReader(f)))
+
+        print(f"Player_Status table populated: {len(players)} rows")
 
